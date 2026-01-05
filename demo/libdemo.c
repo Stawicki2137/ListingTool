4660d0dec5c0e3907fe66231b46be79e6f79fd6a
mateusz2137 <mateusz.stawicki212@gmail.com>
2026-01-05 23:22:33 +0100

    fix: demo

--- demo/libdemo.c
+++ demo/libdemo.c
@@ -4,4 +4,4 @@ int add(int a, int b) { return a + b; }
 __attribute__((visibility("default")))
 int mul(int a, int b) { return a * b; }
 
-static int hidden(int x) { return x + 1; } // nie powinno być eksportowane
+static int hidden(int x) { return x + 1; } 
